23825ae23a1c28e5f0ff138f28e0549e4c4d4d67
peterson-augusto <petersonaugusto001@gmail.com>
2022-10-21 12:29:07 -0300

    commit

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
-from flask import Flask, request, jsonify, render_template
+from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
-import json
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+DB_USER = os.environ.get("DB_USER")
+DB_PASSWORD = os.environ.get('DB_PASSWORD')
+DB_PORT = os.environ.get('DB_PORT')
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql+psycopg2://postgres:86518995@localhost:5432/postgres'
+app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@localhost:{DB_PORT}/postgres'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
@@ -57,7 +63,7 @@ def create_user():
     new_user = User(f_name, l_name, email, genre, birthdata, password)
     db.session.add(new_user)
     db.session.commit()
-    return user_schema.jsonify(new_user), render_template('/cadastro.html')
+    return user_schema.jsonify(new_user)
 
 
     
